f4581e42915012f0dc1215dbb4f23487d42967b9
mohammadkazem harandi <108237484+mohammadkahr@users.noreply.github.com>
2023-04-12 18:09:18 +0430

    This code just works, I call this v1.

--- main.c
+++ main.c
@@ -6,7 +6,13 @@
 #include <stdbool.h>
 
 #define TABLE_SIZE 1000
+#define RTYPE 0
+#define ITYPE 1
+#define JTYPE 2
 
+#define RTYPE 0
+#define ITYPE 1
+#define JTYPE 2
 // methods:
 
 int fillSymTab(struct symbolTable *symT,FILE *inputFile);
@@ -16,6 +22,8 @@ int findSymbolTableLength(FILE *file);
 void duplicatedLabel(struct symbolTable *symbolTable, int symbolTabLength);
 void duplicatedLabel2(struct symbolTable *symbolTable, int symbolTabLength);
 int findOpCode(char *token2, char *instructions[]);
+void result(int op, struct instruction *currentInstruction, struct symbolTable *symbolTable,
+            char hex_table[16], char lower[5], char *token2, int symbolTabLength, int programCounter, int line);
 
 int main(int argc, char **argv){
     // Written by Mohammad._.hr
@@ -90,26 +98,72 @@ int main(int argc, char **argv){
             if (op != -1) {
                 // do the result and write in the file
 
-//                result(op, pSymTab, symTabLen, hexTable,
-//                       lower, token, symTabLen, programCounter);
-//                printf("%d\n", pSymTab->value);
-//                fprintf(machp, "%d\n", currentInstruction->intValueInstruction);
+                result(op, currInst, pSymTab,
+                       hexTable, lower, token, symTabLen,
+                       programCounter , lineCounterInWhile);
+                printf("%d\n", pSymTab->value);
+                fprintf(machp, "%d\n", currInst->intInst);
             }
             else {
-                puts("ERROR! exit(1) : Invalid Opcode");
-                printf("LINE ERROR : ");
+                printf("Invalid Opcode, LINE ERROR : ");
                 printf("%d ", lineCounterInWhile + 1);
                 exit(1);
             }
         }
+        // line with label
+        else {
+            Boolean isInst = FALSE;
+            token = strtok(line2, "\t ");
+            token = strtok(NULL, "\t\n ");
+            for (int op = 0; op < 15; op++) {
+                if (strcmp(instructions[op], token) == 0) {
+                    isInst = TRUE;
+                    // do the result and write in the file
 
+//                    result(op, currentInstruction, symbolTable, hex_table, lower, token, symbolTabLength,
+//                           programCounter);
+//                    printf("%d\n", currentInstruction->intValueInstruction);
+//                    fprintf(machp, "%d\n", currentInstruction->intValueInstruction);
+                    result(op, currInst, pSymTab,
+                           hexTable, lower, token, symTabLen,
+                           programCounter , lineCounterInWhile);
+                    printf("%d\n", pSymTab->value);
+                    fprintf(machp, "%d\n", currInst->intInst);
+                }
+            }
+            if (isInst == FALSE) {
+                if (strcmp(token, ".fill") == 0) {
+                    token = strtok(NULL, "\t\n ");
+                    int found2 = 0;
+                    int count = 0;
+                    for (int i = 0; i < symTabLen; i++) {
+                        if (strcmp(pSymTab[i].symbol, token) == 0) {
+                            found2 = 1;
+                            count = i;
+                        }
+                    }
+                    if (found2) {
+                        currInst->imm = pSymTab[count].value;
+                    } else if (isalpha(token[0])) {
+                        printf("Undefined Label, LINE ERROR : ");
+                        printf("%d ", lineCounterInWhile + 1);
+                        exit(1);
+                    } else {
+                        currInst->imm = atoi(token);
+                    }
+                    currInst->intInst = currInst->imm;
+                    printf("%d\n", currInst->intInst);
+                    fprintf(machp, "%d\n", currInst->intInst);
 
-
-
-
-        lineCounterInWhile++;
+                } else {
+                    printf("Invalid Opcode, LINE ERROR : ");
+                    printf("%d ", lineCounterInWhile + 1);
+                    exit(1);
+                }
+            }
+            lineCounterInWhile++;
+        }
     }
-
 
     fclose(assp);
     fclose(machp);
@@ -117,6 +171,8 @@ int main(int argc, char **argv){
     printf("done!");
     return 0;
 }
+
+//methods:
 int findSymTabLen(FILE *inputFile) {
     int count = 0;
     int lineSize = 72;
@@ -144,8 +200,7 @@ int fillSymTab(struct symbolTable *symT,FILE *inputFile) {
             token = strtok(line, "\t, ");
             for (int j = 0; j < i; ++j) {
                 if (strcmp(token,symT[j].symbol)==0){
-                    puts("ERROR! exit(1) : Duplicated Label");
-                    printf("LINE ERROR : ");
+                    printf("Duplicated Label, LINE ERROR : ");
                     printf("%d ",lineNo+1);
                     exit(1);
                 }
@@ -197,7 +252,6 @@ void int2hex16(char *lower,int a) {
 }
 
 //add by myself ->  );
-
 int findSymbolTableLength(FILE *file) {
     int count = 0;
     char *line = (char *) malloc(72);
@@ -281,4 +335,179 @@ int findOpCode(char *token2, char *instructions[]) {
     }
     return opcode;
 }
+int findSymbol(int symbolTabLength, struct symbolTable *symbolTable, char *token2) {
+    int count = -1;
+    for (int i = 0; i < symbolTabLength; i++) {
+        if (strcmp(symbolTable[i].symbol, token2) == 0) {
+            count = i;
+        }
+    }
+    return count;
+}
+void result(int op, struct instruction *currentInstruction, struct symbolTable *symbolTable,
+            char hex_table[16], char lower[5], char *token2,
+            int symbolTabLength, int programCounter, int line) {
+    if (op >= 0 && op <= 4) {//R format instruction
+        currentInstruction->instType = RTYPE;
+        currentInstruction->mnemonic = (char*) malloc(5);
+        strcpy(currentInstruction->mnemonic, token2);
+        token2 = strtok(NULL, ",\t\n ");
+        currentInstruction->rd = atoi(token2);
+        token2 = strtok(NULL, ",\t\n ");
+        currentInstruction->rs = atoi(token2);
+        token2 = strtok(NULL, ",\t\n ");
+        currentInstruction->rt = atoi(token2);
+        currentInstruction->PC = currentInstruction->PC + 1;
+        currentInstruction->inst[0] = '0';
+        currentInstruction->inst[1] = hex_table[op];
+        currentInstruction->inst[2] = hex_table[currentInstruction->rs];
+        currentInstruction->inst[3] = hex_table[currentInstruction->rt];
+        currentInstruction->inst[4] = hex_table[currentInstruction->rd];
+        currentInstruction->inst[5] = '0';
+        currentInstruction->inst[6] = '0';
+        currentInstruction->inst[7] = '0';
+        currentInstruction->inst[8] = '\0';
+        int res = hex2int(currentInstruction->inst);
+        currentInstruction->intInst = res;
+    }
+    else if (op >= 5 && op <= 12) {//I format instruction
+        currentInstruction->instType = ITYPE;
+        currentInstruction->mnemonic = (char*) malloc(5);
+        strcpy(currentInstruction->mnemonic, token2);
+        if (strcmp(currentInstruction->mnemonic, "jalr") == 0) {
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rt = atoi(token2);
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rs = atoi(token2);
+            currentInstruction->imm = 0;
+        }
+        else if (strcmp(currentInstruction->mnemonic, "lui") == 0) {
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rt = atoi(token2);
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->imm = atoi(token2);
+            currentInstruction->rs = 0;
+        }
+        else if (strcmp(currentInstruction->mnemonic, "beq") == 0) {
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rs = atoi(token2);
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rt = atoi(token2);
+            token2 = strtok(NULL, ",\t\n ");
+            int count = findSymbol(symbolTabLength, symbolTable, token2);
+            if (count != -1) {
+                currentInstruction->imm = symbolTable[count].value - (programCounter);
+            }
+            else if (isalpha((token2[0]))) {
+                puts("Undefined Label, LINE ERROR : ");
+                printf("%d ", line + 1);
 
+                exit(1);
+            }
+            else {
+                long num = atol(token2);
+                if (num < 65536 && num > -65536) {
+                    num = num - (programCounter);
+                    if (num < 0) {
+                        num += 65536;
+                    }
+                    currentInstruction->imm = num;
+                }
+                else {
+                    puts("invalid Offset, LINE ERROR : ");
+                    printf("%d ", line + 1);
+                    exit(1);//invalid offset
+                }
+            }
+        }
+        else {
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rt = atoi(token2);
+            token2 = strtok(NULL, ",\t\n ");
+            currentInstruction->rs = atoi(token2);
+            token2 = strtok(NULL, ",\t\n ");
+            int count = findSymbol(symbolTabLength, symbolTable, token2);
+            if (count != -1) {
+                currentInstruction->imm = symbolTable[count].value;
+            }
+            else if (isalpha((token2[0]))) {
+                puts("Undefined Label, LINE ERROR :");
+                printf("%d ", line + 1);
+                exit(1);
+            }
+            else {
+                long num = atol(token2);
+                if (num < 65536 && num > -65536) {
+                    if (num < 0 && op != 7) {//op ori is 7
+                        num += 65536;
+                    }
+                    currentInstruction->imm = num;
+                }
+                else {
+                    puts("ERROR! exit(1) : invalid Offset\n");
+                    exit(1);//invalid offset
+                }
+            }
+        }
+        currentInstruction->PC = currentInstruction->PC + 1;
+        currentInstruction->inst[0] = '0';
+        currentInstruction->inst[1] = hex_table[op];
+        currentInstruction->inst[2] = hex_table[currentInstruction->rs];
+        currentInstruction->inst[3] = hex_table[currentInstruction->rt];
+        int2hex16(lower, currentInstruction->imm);
+        currentInstruction->inst[4] = lower[0];
+        currentInstruction->inst[5] = lower[1];
+        currentInstruction->inst[6] = lower[2];
+        currentInstruction->inst[7] = lower[3];
+        currentInstruction->inst[8] = '\0';
+        int res = hex2int(currentInstruction->inst);
+        currentInstruction->intInst = res;
+    }
+    else if (op == 13 || op == 14) {
+        //J format
+        currentInstruction->instType = JTYPE;
+        currentInstruction->mnemonic = (char*) malloc(5);
+        strcpy(currentInstruction->mnemonic, token2);
+        if (strcmp(currentInstruction->mnemonic, "halt") == 0) {
+            currentInstruction->imm = 0;
+        }
+        else {
+            //j
+            token2 = strtok(NULL, ",\t\n ");
+            int count = findSymbol(symbolTabLength, symbolTable, token2);
+            if (count != -1) {
+                currentInstruction->imm = symbolTable[count].value;
+            }
+            else if (isalpha((token2[0]))) {
+                puts("Undefined Label, LINE ERROR : ");
+                printf("%d ", line + 1);
+                exit(1);
+            }
+            else {
+                long num = atol(token2);
+                if (num < 65536 && num > -65536) {
+                    currentInstruction->imm = num;
+                }
+                else {
+                    puts("invalid Offset, LINE ERROR : ");
+                    printf("%d ", line + 1);
+                    exit(1);//invalid offset
+                }
+            }
+        }
+        currentInstruction->PC = currentInstruction->PC + 1;
+        currentInstruction->inst[0] = '0';
+        currentInstruction->inst[1] = hex_table[op];
+        currentInstruction->inst[2] = '0';
+        currentInstruction->inst[3] = '0';
+        int2hex16(lower, currentInstruction->imm);
+        currentInstruction->inst[4] = lower[0];
+        currentInstruction->inst[5] = lower[1];
+        currentInstruction->inst[6] = lower[2];
+        currentInstruction->inst[7] = lower[3];
+        currentInstruction->inst[8] = '\0';
+        int res = hex2int(currentInstruction->inst);
+        currentInstruction->intInst = res;
+    }
+
+}
